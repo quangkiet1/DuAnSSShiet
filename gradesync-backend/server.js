210b88d4f143e57e5f1f37cbacccc24cac872bba
kietzuki <huynhkietzuki@gmail.com>
2026-06-18 15:51:40 +0700

    Update CORS for Vercel deployment

--- gradesync-backend/server.js
+++ gradesync-backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 // Security & Middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: function (origin, callback) { callback(null, true); }, // Cho phép mọi domain (kể cả Vercel)
   credentials: true,
   exposedHeaders: ['Content-Disposition', 'Content-Type', 'Content-Length'],
 }));
